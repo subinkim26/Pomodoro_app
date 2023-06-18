ac8d520f84fa433b9b57d6326684c6bec3a1b2fe
김수빈 <77378559+subinkim26@users.noreply.github.com>
2023-06-18 22:53:12 +0900

    feat : 시간에 따라 채워지는 원형둘래 타이머
    타이머 아직 미완성
    코드 수정 필요

--- app/src/main/java/com/example/pomodoro_app/PomodoroFragment.kt
+++ app/src/main/java/com/example/pomodoro_app/PomodoroFragment.kt
@@ -6,26 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PomodoroFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PomodoroFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+class PomodoroFragment : Fragment(), View.OnClickListener {
+
+
+    private var timeCountInMilliSeconds : Long = 1 * 6000;
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -38,15 +35,6 @@ class PomodoroFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PomodoroFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PomodoroFragment().apply {
@@ -55,5 +43,9 @@ class PomodoroFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
