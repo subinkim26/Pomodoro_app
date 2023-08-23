100d0993bb195004d856584d27802d750b6866e0
김수빈 <77378559+subinkim26@users.noreply.github.com>
2023-08-23 18:52:36 +0900

    fix : gradle 오류
    gradle -> maven 오류 해결
    버튼 연결 작업
    
    Ref : 연결 작업 restart 부분 해결, 타이머 연결

--- app/src/main/java/com/example/pomodoro_app/PomodoroFragment.kt
+++ app/src/main/java/com/example/pomodoro_app/PomodoroFragment.kt
@@ -1,10 +1,15 @@
 package com.example.pomodoro_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,22 +22,44 @@ class PomodoroFragment : Fragment(), View.OnClickListener {
 
     private var timeCountInMilliSeconds : Long = 1 * 6000;
 
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+    private enum class TimerStatus {
+        STARTED, STOPPED
     }
+
+    private val timerStatus = TimerStatus.STOPPED
+
+    private lateinit var progressBarCircle : ProgressBar
+    private lateinit var editTextMinute : EditText
+    private lateinit var textViewTime : TextView
+    private lateinit var imageViewReset : ImageView
+    private lateinit var imageViewStartStop : ImageView
+    private lateinit var countDownTimer: CountDownTimer
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pomodoro, container, false)
+
+        val view : View = inflater.inflate(R.layout.fragment_pomodoro, container, false)
+
+        initViews(view);
+
+        initListeners();
+
+        return view
     }
+
+    private fun initViews(view : View) {
+        progressBarCircle = view.findViewById(R.id.progressBarCircle)
+        editTextMinute = view.findViewById(R.id.editTextMinute)
+        textViewTime = view.findViewById(R.id.textViewTime)
+        imageViewReset = view.findViewById(R.id.imageViewReset)
+        imageViewStartStop = view.findViewById(R.id.imageViewStartStop)
+    }
+
+
 
     companion object {
         @JvmStatic
